f46fa4221626b0dd0212989bcbb19df99e63019e
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-05-19 19:38:16 -0500

    Creacion de QuoteService

--- app/src/main/java/com/marco/arquitecturami/data/networt/QuoteService.kt
+++ app/src/main/java/com/marco/arquitecturami/data/networt/QuoteService.kt
@@ -1,4 +1,20 @@
 package com.marco.arquitecturami.data.networt
 
+import com.marco.arquitecturami.core.RetrofitHelper
+import com.marco.arquitecturami.data.model.QuoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+
 class QuoteService {
-}
+
+    private val  retrofit = RetrofitHelper.getRetrofit()
+
+    suspend fun getQuotes():List<QuoteModel>{
+        return withContext(Dispatchers.IO) {
+            val response: Response<List<QuoteModel>> = retrofit.create(QuoteApiClient::class.java).getAllQuotes()
+            response.body() ?: emptyList()
+
+           }
+        }
+    }
